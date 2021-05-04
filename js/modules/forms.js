2e541f7d34e8ab347ffbf88be04db91d65bf8a11
adi <52442230+adi-lazaro@users.noreply.github.com>
2021-05-04 14:22:22 +0530

    BFS: Forms: Update this module

--- js/modules/forms.js
+++ js/modules/forms.js
@@ -4,6 +4,8 @@
  */
 function BFSForm ( className ) {
 
+	className = className.replace( /^\./, "" ).trim();
+
 	/*
 	 * ----- Get a reference to the form
 	 */
@@ -35,34 +37,53 @@ BFSForm.getErrorResponse = function getErrorResponse () {
 	return error;
 }
 BFSForm.prototype.disable = function disable ( fn ) {
-	$( this.domForm ).find( "input, select, button" ).prop( "disabled", true );
+	$( this.domForm ).find( "input, textarea, select, button" ).prop( "disabled", true );
 	if ( Object.prototype.toString.call( fn ).toLowerCase() === "[object function]" )
-		fn.call( this );
+		fn.call( this, this.domForm );
 };
 BFSForm.prototype.enable = function enable ( fn ) {
-	$( this.domForm ).find( "input, select, button" ).prop( "disabled", false );
+	$( this.domForm ).find( "input, textarea, select, button" ).prop( "disabled", false );
 	if ( Object.prototype.toString.call( fn ).toLowerCase() === "[object function]" )
-		fn.call( this );
+		fn.call( this, this.domForm );
 };
 BFSForm.prototype.giveFeedback = function giveFeedback ( message ) {
-	$( this.domForm ).find( "[ type = 'submit' ]" ).text( message );
+	var $submitButton = $( this.domForm ).find( "[ type = 'submit' ]" );
+	// Backup the initial label of the button
+	if ( $submitButton.data( "initial-label" ) === void 0 /* i.e. `undefined` */ )
+		$submitButton.data( "initial-label", $submitButton.text() );
+
+	$submitButton.text( message );
+}
+BFSForm.prototype.setSubmitButtonLabel = function setSubmitButtonText ( label ) {
+	var $submitButton = $( this.domForm ).find( "[ type = 'submit' ]" );
+	var label = label || $submitButton.data( "initial-label" );
+	$submitButton.text( label );
 }
 
 BFSForm.prototype.getFieldValue = function getFieldValue ( domField ) {
-	// var elementTag = domField.nodeName.toLowerCase();
-	// var inputType;
-	// if ( elementTag === "input" )
-	// 	inputType = domField.getAttribute( "type" );
-	return domField.value;
+	var elementTag = domField.nodeName.toLowerCase();
+	var value;
+
+	if ( elementTag === "input" ) {
+		var inputType = domField.getAttribute( "type" );
+		if ( inputType === "radio" )
+			value = domField.checked ? domField.value : null;
+		else
+			value = domField.value;
+	}
+	else if ( elementTag === "textarea" ) {
+		value = domField.value;
+	}
+	return value;
 }
 
-BFSForm.prototype.addField = function addField ( name, isRequired, domFields, fn ) {
+BFSForm.prototype.addField = function addField ( name, domFields, fn ) {
 	if ( ! Array.isArray( domFields ) )
 		domFields = [ domFields ];
 	this.fields[ name ] = {
 		domFields: domFields,
 		validateAndAssemble: fn,
-		isRequired: isRequired
+		// isRequired: isRequired
 	};
 };
 
@@ -72,7 +93,7 @@ BFSForm.prototype.getData = function getData () {
 	for ( _key in this.fields ) {
 		var field = this.fields[ _key ];
 		var valueParts = field.domFields.map( this.getFieldValue );
-		var value = field.validateAndAssemble( valueParts, field.isRequired || false );
+		var value = field.validateAndAssemble( valueParts );
 		this.data[ _key ] = value;
 	}
 	return this.data;
